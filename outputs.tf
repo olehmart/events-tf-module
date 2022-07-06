fda308069604417b32450c629a06aed9f8b963fa
Oleh Martushevskyi <oleh.martushevskyi@telus.com>
2022-07-06 11:51:27 -0400

    module

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
 output "cloud_build_topics" {
   value = [for topic in google_pubsub_topic.cloud-builds: topic.id]
-  # value = google_pubsub_topic.cloud-builds.id
 }
 
 output "cloud_deploy_topics" {
   value = [for topic in google_pubsub_topic.cloud-deploy: topic.id]
-  # value = google_pubsub_topic.cloud-deploy.id
 }
 
 output "cloud_build_subscriptions" {
   value = {for subscription in google_pubsub_subscription.cloud-builds-sub: subscription.topic => subscription.id}
-  # value = google_pubsub_subscription.cloud-builds-sub.id
 }
 
 output "cloud_deploy_subscriptions" {
   value = {for subscription in google_pubsub_subscription.cloud-deploy-sub: subscription.topic => subscription.id}
-  # value = google_pubsub_subscription.cloud-deploy-sub.id
 }
 
 output "webhook_receiver_url" {
@@ -28,4 +24,8 @@ output "webhook_cloud_deploy_path" {
 
 output "webhook_cloud_build_path" {
   value = local.cloud_build_events_path
+}
+
+output "instrumentation_sa_email" {
+  value = local.instrumentation_sa_email
 }
